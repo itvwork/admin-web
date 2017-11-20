2a79ed1bf911e43eb3f11aa7b607b1a7b7f9b13c
langjue <xieke76@qq.com>
2017-11-20 10:44:21 +0800

    0.3.6

--- webpack.config.js
+++ webpack.config.js
@@ -75,18 +75,6 @@ module.exports =  {
         plugins: [
             //new ExtractTextPlugin('../style/styles.css'),
 
-
-            new webpack.optimize.UglifyJsPlugin({
-              compress: {
-                warnings: false
-              }
-            }),
-            new webpack.DefinePlugin({
-                'process.env': {
-                    NODE_ENV: '"production"'
-                }
-            })
-
             // new webpack.optimize.UglifyJsPlugin({
             //   compress: {
             //     warnings: false
