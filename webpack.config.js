a0bcd6cb76b810425f527121c24b1ce4c3544d11
langjue <xieke76@qq.com>
2017-11-20 10:35:22 +0800

    0.3.5

--- webpack.config.js
+++ webpack.config.js
@@ -75,6 +75,7 @@ module.exports =  {
         plugins: [
             //new ExtractTextPlugin('../style/styles.css'),
 
+
             new webpack.optimize.UglifyJsPlugin({
               compress: {
                 warnings: false
